6dd59ed50680e4beb09e76cff8bf27cc855578d9
ronan <ronan.lesturgeon@hotmail.com>
2024-01-09 17:28:08 +0100

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mbtnVrai:Button
+    private lateinit var mbtnFaux:Button
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mbtnVrai = findViewById(R.id.btnVrai)
+        mbtnFaux = findViewById(R.id.btnFaux)
+
+
+        mbtnVrai.setOnClickListener {vue:View ->
+            Toast.makeText(this, "Vrai", Toast.LENGTH_LONG).show()
+        }
+        mbtnFaux.setOnClickListener {vue:View ->
+            Toast.makeText(this, "Faux", Toast.LENGTH_LONG).show()        }
     }
 }
