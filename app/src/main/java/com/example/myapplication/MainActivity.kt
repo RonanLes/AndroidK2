8c1f40b35603e71b23947f9b6ff5b93852a1a502
ronan <ronan.lesturgeon@hotmail.com>
2024-01-10 14:52:22 +0100

    push 15h

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,23 +7,50 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
+import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var mbtnVrai:Button
     private lateinit var mbtnFaux:Button
-
-
+    private lateinit var binding: ActivityMainBinding
+    private val mTabQuestions = listOf(
+        Question(R.string.question_1, true),
+        Question(R.string.question_2, false),
+        Question(R.string.question_3, true),
+        Question(R.string.question_4, false),
+        Question(R.string.question_5, true),
+        )
+    private var mIndexQuestion = 0
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mbtnVrai = findViewById(R.id.btnVrai)
-        mbtnFaux = findViewById(R.id.btnFaux)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-        mbtnVrai.setOnClickListener {vue:View ->
-            Toast.makeText(this, "Vrai", Toast.LENGTH_LONG).show()
+        binding.btnVrai.setOnClickListener {vue:View ->
+            checkAnswer(true)
+            majQuestion()
         }
-        mbtnFaux.setOnClickListener {vue:View ->
-            Toast.makeText(this, "Faux", Toast.LENGTH_LONG).show()        }
+
+        binding.btnFaux.setOnClickListener {vue:View ->
+            checkAnswer(false)
+        }
+
+        binding.btnNext.setOnClickListener {vue:View ->
+            mIndexQuestion = (mIndexQuestion + 1) % mTabQuestions.size
+            majQuestion()
+        }
+    }
+    private fun majQuestion(){
+        val questionTextResId = mTabQuestions[mIndexQuestion].textResId
+        binding.tvQuestion.setText(questionTextResId)
+    }
+private fun checkAnswer(userAnswer:Boolean){
+        val correctAnswer = mTabQuestions[mIndexQuestion].answer
+        val messageResId = if (userAnswer == correctAnswer){
+            R.string.correct_toast
+        }else{
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/myapplication/Question.kt
+++ app/src/main/java/com/example/myapplication/Question.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.widget.Button
 import androidx.annotation.StringRes
 
 data class Question (@StringRes val textResId: Int, val answer: Boolean){
+
 
 
 }
